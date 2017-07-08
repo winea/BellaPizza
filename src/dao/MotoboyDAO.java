49cf196907528878284dda95cc90ab54e16226f0
Winea Pasquareli <winy2000@gmail.com>
2017-07-08 19:08:23 -0300

    DAO

---
+++ src/dao/MotoboyDAO.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+import factory.ConnectionFactory;
+import modelo.Motoboy;
+import java.sql.*;
+import java.sql.PreparedStatement;
+/**
+ *
+ * @author Laryssa
+ */
+public class MotoboyDAO {
+    private Connection connection;
+    String nome;
+    
+    public MotoboyDAO(){ 
+        this.connection = new ConnectionFactory().getConnection();
+    } 
+    public void adiciona(Motoboy motoboy){ 
+        String sql = "INSERT INTO motoboy(Nome) VALUES(?)";
+        try { 
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, motoboy.getNomeMotoboy());
+            stmt.execute();
+            stmt.close();
+        } 
+        catch (SQLException u) { 
+            throw new RuntimeException(u);
+        }      
+    }   
+}
