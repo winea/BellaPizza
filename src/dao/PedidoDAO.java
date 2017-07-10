d0d8b83d91d4dfbb0d59e31067c91631c981f07a
Mizuiky <escolaridade@gmail.com>
2017-07-10 13:50:00 -0300

    Tentando criar o combobox da pizza no pedido

--- src/dao/PedidoDAO.java
+++ src/dao/PedidoDAO.java
@@ -8,10 +8,13 @@ import factory.ConnectionFactory;
 import modelo.Cliente;
 import java.sql.*;
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import modelo.Pedido;
+import modelo.Pizza;
 /**
  *
  * @author escol
@@ -45,5 +48,39 @@ public class PedidoDAO {
         return cliente;
     }
     
+    public List<Pizza> getNome() throws SQLException {
+        List<Pizza> listapizzas = new ArrayList<Pizza>();
+        String sql = ("select * from cliente order by nome");
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        Pizza pizza;
+        ResultSet rs = stmt.executeQuery();
+        while (rs.next()) {
+            pizza = new Pizza();
+            pizza.setNome(rs.getString("Nome"));
+            pizza.setCodigo(rs.getInt("Codigo_Pizza"));
+            
+            listapizzas.add(pizza);
+        }
+        return listapizzas;   
+    }
+    
+    public void Finalizar_Salvar(){
+        
+        String sql = "INSERT INTO pedido(Codigo_Cliente,Nome,Telefone,Rua,Numero,Bairro,Complemento)" + "VALUES(?,?,?,?,?,?,?)";
+        try { 
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1,cliente.getCodigo());
+            stmt.setString(2,cliente.getNome());
+            stmt.setInt(3,cliente.getTelefone());
+            stmt.setString(4,cliente.getRua());
+            stmt.setInt(5,cliente.getNumero());
+            stmt.setString(6,cliente.getBairro());
+            stmt.setString(7,cliente.getBairro());
+            stmt.execute();
+        } 
+        catch (SQLException u) { 
+            JOptionPane.showMessageDialog(null,"Erro ao cadastrar tente novamente"+u);
+        }  
+    }
     
 }
